ca17ba479a2f9399c871707460510c8952b1d23c
DESKTOP-22MUN38\admin <anhthunn120@gmail.com>
2017-10-27 16:02:14 +0700

    no message

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlashcardComponent implements OnInit {
 
-  faceA: string;
+  faceA: string = "thu";
+  faceB: string = "anh thu";
 
   constructor() { }
 
